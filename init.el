4495b5383b028178f95fc4941ec65b7af545e92b
LIU Yang <JeremyRobturtle@gmail.com>
2015-07-03 17:09:40 +0800

    move custom code out of init.el

--- init.el
+++ init.el
@@ -20,16 +20,8 @@
 ;; load up all literate org-mode files in this directory
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
+;; store customized code other file
+(setq custom-file "~/.emacs.d/.custom.el")
+(load custom-file)
+
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((no-byte-compile t)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
