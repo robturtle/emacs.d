39eca39d757252cb966b206bde591eadf6114290
LIU Yang <JeremyRobturtle@gmail.com>
2015-10-05 03:51:34 -0400

    no need for fixup python-mode (quit it now)

--- FIX.sh
+++
@@ -1,6 +0,0 @@
-fname='python-mode'
-f=`find elpa -name "${fname}.el"`
-patch -p0 -N --dry-run --silent "$f" < "${fname}".patch 2>/dev/null
-if [[ "$?" == "0" ]]; then
-    patch -p0 -N "$f" < "${fname}".patch
-fi
